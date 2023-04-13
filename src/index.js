6debaaa4705c773587e9ff92f11bddad867d5dbf
Koci H <husnukoci94@gmail.com>
2023-04-13 22:12:25 +0300

    Core is done.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,44 @@
 
-console.log(data);
+for (i = 0; i < data.length; i++) {
+    const ul = document.querySelector('ul');
+    const li = document.createElement('li');
+    li.setAttribute('class', 'card');
+    li.style.listStyleType = 'none';
+    ul.append(li);
+
+    const h2 = document.createElement('h2');
+    h2.setAttribute('class', 'card--title');
+    h2.innerText = data[i].name.charAt(0).toUpperCase() + data[i].name.slice(1)
+    li.append(h2);
+
+    const img = document.createElement('img')
+    img.setAttribute('class', 'card--img');
+    li.append(img);
+    img.setAttribute('width', '256');
+    img.src = data[i].sprites.other["official-artwork"].front_default
+    li.append(img);
+
+    const newUl = document.createElement('ul');
+    newUl.setAttribute('class', 'card--text')
+
+    li.append(newUl)
+    
+    for (j = 0; j < data[j].stats.length; j++) {
+    const newLi = document.createElement('li');
+    newLi.innerText = data[i].stats[j].stat.name.toUpperCase() + ": " + data[i].stats[j].base_stat;
+    newLi.style.listStyleType = 'none';
+    newUl.append(newLi);
+    }
+}
+
+
+
+
+
+
+
+
 
 //You can start simple and just render a single 
 //pokemon card from the first element
-console.log(data[0]);
+
